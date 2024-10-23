236adbf2d4ac8398839a4eda0e18f512f3d04e61
kitsuiwebster <raphmartin1999@gmail.com>
2024-10-23 21:02:32 +0200

    feat: style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 import { NotYetComponent } from './components/pages/not-yet/not-yet.component';
+import { TeamComponent } from './components/pages/team/team.component';
+import { ServicesComponent } from './components/pages/services/services.component';
+import { ArtistsComponent } from './components/pages/artists/artists.component';
 
 export const routes: Routes = [
 //   { path: '', component: HomeComponent },
@@ -13,6 +16,9 @@ export const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'not-yet', component: HomeComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'artists', component: ArtistsComponent },
   { path: '**', redirectTo: 'not-found' } 
 ];
 
