f2f36020d00782fdebe19436727b8d3803d806e7
kitsuiwebster <raphmartin1999@gmail.com>
2024-12-01 12:38:36 +0100

    feat: form validity conditions

--- src/app/components/pages/getintouch/getintouch.component.ts
+++ src/app/components/pages/getintouch/getintouch.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-getintouch',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './getintouch.component.html',
-  styleUrl: './getintouch.component.scss'
+  styleUrls: ['./getintouch.component.scss'],
 })
 export class GetintouchComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.pattern(/^\+?\d{10,15}$/)]],
+      instagram: [''],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+      privacyPolicy: [false, [Validators.requiredTrue]],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      return;
+    }
+    console.log('Form submitted', this.contactForm.value);
+  }
 }
