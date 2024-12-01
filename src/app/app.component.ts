c6f242386f627b95e6987641b5143ad6bafe3b84
kitsuiwebster <raphmartin1999@gmail.com>
2024-12-01 08:51:14 +0100

    fix: router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   template: '<app-layout></app-layout>',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
