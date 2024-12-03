adc199c31b05fffd893fda74ef9969bbaf97a28a
kitsuiwebster <raphmartin1999@gmail.com>
2024-12-03 11:39:08 +0100

    feat: i18n implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ["./src/**/*.html", "./src/**/*.ts"],
+    content: [
+      './src/**/*.{html,ts}'
+    ],
   },
   theme: {
     extend: {
